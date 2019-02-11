99e18e946394e6502f20084cfe72a66b79e5d3ef
vic3r <barcelonamane@hotmail.com>
2019-02-10 23:30:04 -0600

    Modify the Neighborhood properties

--- internal/models/neighborhood.go
+++ internal/models/neighborhood.go
@@ -2,9 +2,9 @@ package models
 
 // Neighborhood is a cluster defintion in a region (city)
 type Neighborhood struct {
-	ID        int32
-	Name      string
-	Latitude  float32
-	Longitude float32
-	Stations  []*Station
+	ID         int32
+	Name       string
+	Latitudes  []float32
+	Longitudes []float32
+	Stations   []*Station
 }
