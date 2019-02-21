81d9ac2db2aeaf4898726e691aaed7d2c4f561e6
vic3r <victor@MacBook-de-Crabi.local>
2019-02-21 02:14:05 -0600

    Refactor the models already defined

--- internal/models/station.go
+++ internal/models/station.go
@@ -4,9 +4,8 @@ import "time"
 
 // Station defines a bike station with its current properties
 type Station struct {
-	ID            int32         `json:"id"`
-	Municipality  string        `json:"municipality"`
-	Neighborhood  *Neighborhood `json:"neighborhood"`
-	BikesQuantity int32         `json:"bikes_quantity"`
-	Date          time.Time     `json:"date"`
+	ID             int32     `json:"id"`
+	NeighborhoodID int32     `json:"neighborhood"`
+	BikesQuantity  int32     `json:"bikes_quantity"`
+	Date           time.Time `json:"date"`
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -2,6 +2,6 @@ package models
 
 // User defines a person which is MiBici's user
 type User struct {
-	Age    int32  `json:"age"`
-	Gender string `json:"gender"`
+	Age    int32  `json:"age,omitempty"`
+	Gender string `json:"gender,omitempty"`
 }
