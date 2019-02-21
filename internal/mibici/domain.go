40fea4b8eefb18c829a0bb076cad21da888d1e61
vic3r <victor@MacBook-de-Crabi.local>
2019-02-21 02:13:16 -0600

    Add domain for mibici service

--- internal/mibici/domain.go
+++ internal/mibici/domain.go
@@ -1,1 +1,14 @@
 package mibici
+
+type domain struct {
+	controller interface{}
+}
+
+func newDomain(usecases UseCases) (*domain, error) {
+	controller, err := newController(usecases)
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain{controller}, nil
+}
