5d6cba695be9df61b7f0f36a18bbf7382d0d21cc
vic3r <victor@MacBook-de-Crabi.local>
2019-02-21 15:18:14 -0600

    Add redis logic for retrieving neighborhood

--- internal/mibici/usecases/storage/redis/redis.go
+++ internal/mibici/usecases/storage/redis/redis.go
@@ -1,32 +1,66 @@
 package redis
 
 import (
-	r "github.com/go-redis/redis"
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-redis/redis"
 	"github.com/vic3r/smart-cities-back/internal/mibici/usecases/storage"
 	"github.com/vic3r/smart-cities-back/internal/models"
 )
 
 // Redis is a struct where the client lies
-type redis struct {
-	client *r.Client
+type redisClient struct {
+	client *redis.Client
 }
 
-var _ storage.Storage = &redis{}
+var _ storage.Storage = &redisClient{}
 
-// New creates new instance from a redis client
-func New(client *r.Client) (storage.Storage, error) {
+// New creates new instance from a redisClient client
+func New(client *redis.Client) (storage.Storage, error) {
 
-	return &redis{client}, nil
+	return &redisClient{client}, nil
 }
 
 // GetNeighborhoodByID returns a neighborhood
-func (r *redis) GetNeighborhoodByID(int) (*models.Neighborhood, error) {
-	// TODO: Handle entitity in redis
-	return nil, nil
+func (r *redisClient) GetNeighborhoodByID(neighborhoodID int) (*models.Neighborhood, error) {
+	// Query to retrieve a neighborhood
+	neighborhoodQuery := fmt.Sprintf("neighborhood:%d", neighborhoodID)
+	response := redis.NewStringCmd(r.client.Do("GET", neighborhoodQuery))
+
+	// parse response to byte array[]
+	byteResponse, err := response.Bytes()
+	if err != nil {
+		return nil, fmt.Errorf("not possible to parse response: %v", err)
+	}
+
+	nieghborhoodResponse := &models.Neighborhood{}
+	// parse byte array response to json
+	err = json.Unmarshal(byteResponse, nieghborhoodResponse)
+	if err != nil {
+		return nil, fmt.Errorf("not possible to unmarshal response: %v", err)
+	}
+
+	return nieghborhoodResponse, nil
 }
 
 // GetListNeighborhoods returns a neighborhood list
-func (r *redis) GetListNeighborhoods() ([]*models.Neighborhood, error) {
-	// TODO: Handle entities in redis
-	return nil, nil
+func (r *redisClient) GetListNeighborhoods() ([]*models.Neighborhood, error) {
+	// Query to retrieve a list of neighborhoods
+	response := redis.NewStringCmd(r.client.Do("GET", "neighborhood"))
+
+	// parse response to byte array[]
+	byteResponse, err := response.Bytes()
+	if err != nil {
+		return nil, fmt.Errorf("not possible to parse response: %v", err)
+	}
+
+	nieghborhoodsResponse := []*models.Neighborhood{}
+	// parse byte array response to json
+	err = json.Unmarshal(byteResponse, nieghborhoodsResponse)
+	if err != nil {
+		return nil, fmt.Errorf("not possible to unmarshal response: %v", err)
+	}
+
+	return nieghborhoodsResponse, nil
 }
