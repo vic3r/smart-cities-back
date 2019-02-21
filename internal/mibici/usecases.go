fa29a0975393b50b4ca33ca42cdf8e79f743710a
vic3r <victor@MacBook-de-Crabi.local>
2019-02-21 02:11:45 -0600

    Create use cases struct with validation in dummy cases

--- internal/mibici/usecases.go
+++ internal/mibici/usecases.go
@@ -1,1 +1,11 @@
 package mibici
+
+import (
+	"github.com/vic3r/smart-cities-back/internal/models"
+)
+
+// UseCases is an interface for the usecases
+type UseCases interface {
+	GetNeighborhood(neighborhoodID string) (*models.Neighborhood, error)
+	GetNeighborhoods() ([]*models.Neighborhood, error)
+}
--- internal/mibici/usecases/usecases.go
+++ internal/mibici/usecases/usecases.go
@@ -1,1 +1,50 @@
 package usecases
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/vic3r/smart-cities-back/internal/mibici"
+	"github.com/vic3r/smart-cities-back/internal/mibici/usecases/storage"
+	"github.com/vic3r/smart-cities-back/internal/models"
+)
+
+// defaultUseCases is a usecases implementation
+type defaultUseCases struct {
+	storage storage.Storage
+}
+
+var _ mibici.UseCases = &defaultUseCases{}
+
+// New creates a new set of default use cases
+func New(storage storage.Storage) (mibici.UseCases, error) {
+	if storage == nil {
+		return nil, errors.New("nil storage")
+	}
+
+	return &defaultUseCases{storage: storage}, nil
+}
+
+// GetNeighborhood returns a neighborhood
+func (u *defaultUseCases) GetNeighborhood(neighborhoodID string) (*models.Neighborhood, error) {
+	nbhoodID, err := strconv.Atoi(neighborhoodID)
+	if err != nil {
+		return nil, errors.New("not possible to parse neighborhood_id")
+	}
+	neighborhood, err := u.storage.GetNeighborhoodByID(nbhoodID)
+	if err != nil {
+		return nil, err
+	}
+
+	return neighborhood, nil
+}
+
+// GetNeighborhoods returns a list of neighborhoods
+func (u *defaultUseCases) GetNeighborhoods() ([]*models.Neighborhood, error) {
+	neighborhood, err := u.storage.GetListNeighborhoods()
+	if err != nil {
+		return nil, err
+	}
+
+	return neighborhood, nil
+}
